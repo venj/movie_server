eccd553260b1a84bab9eafeee04e6997d08e5846
Venj <ersaclarke@gmail.com>
2013-06-07 22:47:44 +0800

    Updated app2.rb to make it work with torrents and lixian; Merge app2.rb to app.rb; keep app2.rb for local test.

--- app.rb
+++ app.rb
@@ -9,12 +9,32 @@ def get_public_folder
   YAML.load(open(File.join(File.dirname(__FILE__), 'server_conf.yml')).read)["public_folder"]
 end
 
+alias :get_torrents_folder :get_public_folder
+
+def torrent_with_pic(pic)
+  pic_name = File.basename(pic, ".jpg")
+  pic_dir = File.dirname(pic)
+  possible_tr_name = File.join(pic_dir, "#{pic_name}.torrent")
+  if File.exists?(possible_tr_name)
+    return possible_tr_name
+  else
+    tr_base = pic_name.gsub(/(201\d_\d\d-\d\d?-4)\./, '\1_')
+    tr_name = File.join(pic_dir, "#{tr_base}.torrent")
+    if File.exists?(tr_name)
+      return tr_name
+    else
+      return nil
+    end
+  end
+end
+
 set :public_folder, get_public_folder
 
 before do
   content_type 'text/json'
 end
 
+# Movie live cast
 get "/" do
   movies = []
   cd get_public_folder do
@@ -33,3 +53,46 @@ get "/info/:file" do
   stat = File.stat(File.join(get_public_folder, f))
   {file: f, size: stat.size, atime: stat.atime, mtime: stat.mtime, ctime: stat.ctime}.to_json
 end
+
+# Torrents related
+get "/torrents/:page" do
+  page = params[:page].to_i
+  return [] if page == 0
+  torrents = []
+  cd get_torrents_folder do
+    torrents = Dir["*.jpg"][0 * page ... TORRENT_BATCH * page]
+  end
+  return torrents.to_json
+end
+
+get "/search/:keyword" do
+  keyword = params[:keyword]
+  pics = []
+  cd get_torrents_folder do
+    pics = Dir["**/*.jpg"].select { |f| f.index keyword }
+  end
+  return pics.to_json
+end
+
+get "/lx/:file" do
+  f = params[:file]
+  if f =~ /%252F/  # Linux server
+    f.gsub!("%252F", "/")
+  else             # OS X server
+    f.gsub!("%2F", "/")
+  end
+  cd get_torrents_folder do
+    result = %x[/usr/local/bin/my lx add #{torrent_with_pic f}]
+    if result =~ /completed/
+      status = "completed"
+    elsif result =~ /waiting/
+      status = "waiting"
+    elsif result =~ /downloading/
+      status = "downloading"
+    else
+      status = "failed or unknown"
+    end
+    return {status: status}.to_json
+  end
+end
+
--- app2.rb
+++ app2.rb
@@ -11,6 +11,23 @@ def get_torrents_folder
   YAML.load(open(File.join(File.dirname(__FILE__), 'server_conf.yml')).read)["torrents_folder"]
 end
 
+def torrent_with_pic(pic)
+  pic_name = File.basename(pic, ".jpg")
+  pic_dir = File.dirname(pic)
+  possible_tr_name = File.join(pic_dir, "#{pic_name}.torrent")
+  if File.exists?(possible_tr_name)
+    return possible_tr_name
+  else
+    tr_base = pic_name.gsub(/(201\d_\d\d-\d\d?-4)\./, '\1_')
+    tr_name = File.join(pic_dir, "#{tr_base}.torrent")
+    if File.exists?(tr_name)
+      return tr_name
+    else
+      return nil
+    end
+  end
+end
+
 set :public_folder, get_torrents_folder
 
 before do
@@ -26,3 +43,35 @@ get "/torrents/:page" do
   end
   return torrents.to_json
 end
+
+get "/search/:keyword" do
+  keyword = params[:keyword]
+  pics = []
+  cd get_torrents_folder do
+    pics = Dir["**/*.jpg"].select { |f| f.index keyword }
+  end
+  return pics.to_json
+end
+
+get "/lx/:file" do
+  f = params[:file]
+  if f =~ /%252F/  # Linux server
+    f.gsub!("%252F", "/")
+  else             # OS X server
+    f.gsub!("%2F", "/")
+  end
+  cd get_torrents_folder do
+    result = %x[/usr/local/bin/my lx add #{torrent_with_pic f}]
+    if result =~ /completed/
+      status = "completed"
+    elsif result =~ /waiting/
+      status = "waiting"
+    elsif result =~ /downloading/
+      status = "downloading"
+    else
+      status = "failed or unknown"
+    end
+    return {status: status}.to_json
+  end
+end
+
