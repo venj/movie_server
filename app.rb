a14fda1cbbd56174995f74e1233b1d057a6e625c
Venj Chu <ersaclarke@gmail.com>
2013-07-02 15:22:32 +0800

    Refactored the code and updated the configuration to make loading torrents *much* faster.

--- app.rb
+++ app.rb
@@ -8,19 +8,27 @@ require "shellwords"
 require "fileutils"
 include FileUtils
 
-def get_public_folder
-  YAML.load(open(File.join(File.dirname(__FILE__), 'server_conf.yml')).read)["public_folder"]
+class AppConfig
+  def initialize
+    @vars = YAML.load(open(File.join(File.dirname(__FILE__), 'server_conf.yml')).read)
+  end
+  
+  def public_folder
+    @vars["public_folder"]
+  end
+  
+  def lx_command
+    @vars["lixian_command"]
+  end
+  
+  def max_pic_size
+    @vars["max_pic_size"].to_i * 1024
+  end
+  
+  def relative_folders
+    @vars["relative_folders"]
+  end
 end
-
-def get_lx_command
-  YAML.load(open(File.join(File.dirname(__FILE__), 'server_conf.yml')).read)["lixian_command"]
-end
-
-def get_max_pic_size
-  YAML.load(open(File.join(File.dirname(__FILE__), 'server_conf.yml')).read)["max_pic_size"].to_i * 1024
-end
-
-alias :get_torrents_folder :get_public_folder
 
 def torrent_with_pic(pic)
   pic_name = File.basename(pic, ".jpg")
@@ -48,16 +56,17 @@ def date_with_pic(pic)
   pic.match(/(201\d_\d\d-\d\d?)/).to_a[1] || pic.match(/(\[\d\-\d\d\]最新BT合集)/).to_a[1]
 end
 
-set :public_folder, get_public_folder
-
 before do
   content_type 'text/json'
 end
 
+config = AppConfig.new
+set :public_folder, config.public_folder
+
 # Movie live cast
 get "/" do
   movies = []
-  cd get_public_folder do
+  cd config.public_folder do
     movies = Dir["**/*"].select { |f| ["mp4", "m4v", "mov"].include? f.split(".").last.downcase }.sort.to_json
   end
   movies
@@ -70,7 +79,7 @@ get "/info/:file" do
   else             # OS X server
     f.gsub!("%2F", "/")
   end
-  stat = File.stat(File.join(get_public_folder, f))
+  stat = File.stat(File.join(config.public_folder, f))
   {file: f, size: stat.size, atime: stat.atime, mtime: stat.mtime, ctime: stat.ctime}.to_json
 end
 
@@ -81,7 +90,7 @@ delete "/remove/:file" do
   else             # OS X server
     f.gsub!("%2F", "/")
   end
-  cd get_public_folder do
+  cd config.public_folder do
     if File.exists?(f)
       %x[rm -f #{f.shellescape}]
     end
@@ -91,23 +100,26 @@ end
 
 # Torrents related
 get "/torrents" do
-  dates = []
-  tr_folder = get_torrents_folder
-  cd tr_folder do
-    Dir["**/*.jpg"].each do |p|
-      d = date_with_pic(p)
-      dates << d unless dates.index d
+  datelist = []
+  folders = config.relative_folders
+  cd config.public_folder do
+    cd folders[0] do
+      regex = /(\d{4}\/\d{2}-\d{1,2})(-\d\/)?$/
+      selected = open(".finished").readlines.to_a.select { |u| u.strip =~ regex }
+      datelist = selected.map { |u| regex.match(u)[1].gsub("/", "_") }
+    end
+    cd folders[1] do
+      datelist += Dir["**"].select {|f| f != "tu.rb" }.reverse
     end
   end
-  dates.compact! # Why the trailing nil?
-  return dates.sort { |x, y| (x.index("[") != y.index("[")) ? (x <=> y) * -1 : x <=> y }.reverse.to_json
+  return datelist.sort { |x, y| (x.index("[") != y.index("[")) ? (x <=> y) * -1 : x <=> y }.reverse.to_json
 end
 
 get "/search/:keyword" do
   keyword = params[:keyword]
   pics = []
-  max_pic_size = get_max_pic_size
-  cd get_torrents_folder do
+  max_pic_size = config.max_pic_size
+  cd config.public_folder do
     pics = Dir["**/*"].select do |f|
       if ["jpg", "gif", "png"].index(f.split(".").last.downcase)
         (f.index(keyword) && (File.stat(f).size < max_pic_size))
@@ -124,8 +136,8 @@ get "/lx/:file/:async" do
   else             # OS X server
     f.gsub!("%2F", "/")
   end
-  lx_command = get_lx_command
-  cd get_torrents_folder do
+  lx_command = config.lx_command
+  cd config.public_folder do
     if params[:async] == "1"
       fork {
         exec "#{lx_command} add #{torrent_with_pic f}"
