635d2adb0b560f79b75bd8e24c33a1093207d86d
Venj <ersaclarke@gmail.com>
2017-06-01 14:18:53 +0800

    Fix 5.

--- app.rb
+++ app.rb
@@ -227,11 +227,13 @@ get '/torrents' do
 
         if include_stats
           items_counts += list.map do |d|
+            c = 0
             cd d do
-              Dir["*"].select do |f|
+              c = Dir["*"].select do |f|
                 ["jpg", "gif", "png", "bmp", "jpeg"].index(f.split(".").last.downcase)
               end.count
             end
+            c
           end
         end
         datelist += list
